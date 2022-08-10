ee8ae3d7d47f83be462941b6d200c9178e96ab6e
Ryan Ashcraft <ryan@ryanashcraft.me>
2022-08-09 17:10:38 -0700

    Explicitly declare supported platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Reachability",
+    platforms: [
+        .iOS("13.0"),
+        .macOS("11.0"),
+        .tvOS("13.0"),
+    ],
     products: [
         .library(
             name: "Reachability",
